3796b90e1663e3bb4cc1f352fa295afc59154a8d
ulunj <b15145456@gmail.com>
2020-09-08 20:46:57 +0800

    un solved  for problem 3 method 2

--- LongestSubstringWithoutRepeatingCharacters.py
+++ LongestSubstringWithoutRepeatingCharacters.py
@@ -1,6 +1,9 @@
 s = "abcabcbb"
 substrList = []
 newList=[]
+
+"""""""""""Solution 1"""""""""""""""
+
 def findAllSubString(str):
     res = [str[i: j] for i in range(len(str)) 
           for j in range(i + 1, len(str) + 1)]
@@ -27,4 +30,28 @@ for i in substrList:
     else:
         pass
 print(findMax(newList))
+
+
+"""""""""""Solution 2"""""""""""""""
+
+# 1.檢查dict是否存在當前字符，如果存在更新pointer位置
+# 2.計算max
+# 3.更新dict
+# https://www.youtube.com/watch?v=ZlCxw1VUYj0&t=316s&ab_channel=XiangyuCai
+def lengthOfLongestSubstring(s):
+    point = 0
+    dic={}
+    max = 0
+    for i in range(len(s)):
+        if (s[i] in dic):
+            dic[s[i]] = i
+            point = dic[s[i]]-1
+        else:
+            dic[s[i]] = i
+            if ((i - point + 1)>max):
+                max = i - point + 1
+    return max
+
+s = "abcabcbb"
+print(lengthOfLongestSubstring(s))
 
